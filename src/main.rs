ec7d9059816d82d19aa938dd36252b39488363c6
Kiran Shila <me@kiranshila.com>
2022-06-08 14:40:37 -0700

    Add the ping check

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod args;
 mod handlers;
 
 use clap::Parser;
-use katcp::prelude::*;
+use katcp::{messages::core::*, prelude::*};
 use std::{collections::HashMap, error::Error, net::IpAddr, path::PathBuf};
 use std::{fmt::Debug, net::SocketAddr};
 use tokio::task;
@@ -14,7 +14,7 @@ use tokio::{
     },
     sync::mpsc::{unbounded_channel, UnboundedReceiver},
 };
-use tracing::debug;
+use tracing::{debug, error, trace};
 
 use args::*;
 use handlers::*;
@@ -25,77 +25,65 @@ struct State {
     writer: OwnedWriteHalf,
 }
 
-// async fn read_version_connect(state: &mut State) -> VersionConnect {
-//     let line = state
-//         .lines
-//         .next_line()
-//         .await
-//         .expect("Error awaiting version connect line")
-//         .unwrap(); // Why do I need an unwap here?
-//     line.as_str()
-//         .try_into()
-//         .expect("Error deserializing version connect message")
-// }
+async fn make_request<T>(state: &mut State, request: T) -> Result<Vec<T>, String>
+where
+    T: KatcpMessage + Debug,
+    <T as TryFrom<Message>>::Error: Debug,
+{
+    // Serialize and send request
+    let request_msg = request
+        .to_message(None)
+        .expect("Could not serialize request to a KATCP message");
+    if request_msg.kind() != MessageKind::Request {
+        return Err("We tried to send a request message that wasn't actually a request".to_owned());
+    }
+    trace!(?request);
+    state
+        .writer
+        .write_all(request_msg.to_string().as_bytes())
+        .await
+        .expect("Error writting bytes to TCP connection");
+    let mut messages = vec![];
+    loop {
+        match state.unhandled_incoming_messages.recv().await {
+            Some(v) => {
+                trace!(?v);
+                match v.kind() {
+                    MessageKind::Request => unreachable!(),
+                    MessageKind::Inform => messages.push(
+                        v.try_into()
+                            .expect("Error processing incoming inform message"),
+                    ),
+                    MessageKind::Reply => {
+                        messages.push(
+                            v.try_into()
+                                .expect("Error processing incoming reply message"),
+                        );
+                        break;
+                    }
+                }
+            }
+            None => panic!("The channel we were expecting messages from has been closed"),
+        }
+    }
+    Ok(messages)
+}
 
-// async fn send_request<K: KatcpMessage + std::fmt::Debug>(
-//     request: K,
-//     state: &mut State,
-// ) -> (Vec<K>, Option<Fpga>)
-// where
-//     <K as std::convert::TryFrom<katcp::prelude::Message>>::Error: std::fmt::Debug,
-// {
-//     // Serialize and send request
-//     let request_msg = request
-//         .to_message(None)
-//         .expect("Could not serialize request to a KATCP message");
-//     let request_name = request_msg.name();
-//     state
-//         .writer
-//         .write_all(request_msg.to_string().as_bytes())
-//         .await
-//         .expect("Error writting bytes to TCP connection");
-
-//     // Containers for collected messages
-//     let mut informs_and_reply = vec![];
-//     // We may recieve a number of FPGA status updates (that seemss to just randomly happen)
-//     // We only care about the last one (the current FPGA status)
-//     let mut status_updates: Vec<Fpga> = vec![];
-
-//     // Keep reading lines
-//     while let Some(line) = state.lines.next_line().await.unwrap() {
-//         // We expect either log informs, message informs, or replys
-//         let raw: Message = line.as_str().try_into().expect("Malformed KATCP message");
-//         let new_msg_type = raw.name();
-//         if new_msg_type != request_name && new_msg_type != "log" && new_msg_type != "fpga" {
-//             panic!("Got unexpected KATCP message: {}", raw.name());
-//         }
-//         // Deal with the three cases
-//         match new_msg_type.as_str() {
-//             "log" => handle_log(raw.try_into().expect("Could not deserialize log message")),
-//             "fpga" => {
-//                 let status = raw.try_into().expect("Could not deserialize fpga stauts");
-//                 trace!(?status);
-//                 status_updates.push(status);
-//             }
-//             _ => match raw.kind() {
-//                 MessageKind::Request => unreachable!(),
-//                 MessageKind::Reply => {
-//                     // Push the last reply and break
-//                     let reply = raw.try_into().expect("Could not deserialize KATCP reply");
-//                     trace!(?reply);
-//                     informs_and_reply.push(reply);
-//                     break;
-//                 }
-//                 MessageKind::Inform => {
-//                     let inform = raw.try_into().expect("Could not deserialize KATCP infrom");
-//                     trace!(?inform);
-//                     informs_and_reply.push(inform);
-//                 }
-//             },
-//         }
-//     }
-//     (informs_and_reply, status_updates.last().cloned())
-// }
+async fn ping(state: &mut State) {
+    match make_request(state, Watchdog::Request).await {
+        Ok(v) => {
+            if let Watchdog::Reply(GenericReply::Ok) = v.get(0).unwrap() {
+                debug!("Got a successful ping!");
+            } else {
+                panic!("Got a bad ping, we're bailing");
+            }
+        }
+        Err(e) => {
+            println!("{}", e);
+            panic!("Ping errored: we're bailing");
+        }
+    }
+}
 
 // /// Returns a vector of bof-files present on the device
 // async fn get_bofs(state: &mut State) -> Vec<String> {
@@ -155,21 +143,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .into_split();
     // Startup dispatcher
     dispatch_katcp_messages(tx, reader, make_dispatchers()).await;
+    // Setup the program state
+    let mut state = State {
+        unhandled_incoming_messages: rx,
+        writer,
+    };
+    // Do an initial ping to make sure we're actually connected
+    ping(&mut state).await;
     Ok(())
-
-    // // Setup the program state
-    // let mut state = State {
-    //     lines: BufReader::new(reader).lines(),
-    //     writer,
-    // };
-
-    // // Read the first three informs that give us system information
-    // let device_lib = read_version_connect(&mut state).await;
-    // let device_protocol = read_version_connect(&mut state).await;
-    // let device_kernel = read_version_connect(&mut state).await;
-    // debug!(?device_lib);
-    // debug!(?device_protocol);
-    // debug!(?device_kernel);
     // // Perform the action
     // match args.command {
     //     Command::Load { path, force } => {
