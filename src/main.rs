60f91e4f23c8dd39237083dbd09162c2cac055cb
Kiran Shila <me@kiranshila.com>
2022-06-07 18:05:34 -0700

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use clap::{Parser, Subcommand};
 use katcp::{
-    messages::{core::VersionConnect, log::Log},
+    messages::{
+        core::VersionConnect,
+        log::{Log, LogLevel},
+    },
     prelude::*,
 };
-use katcp_casper::Listbof;
-use std::fmt::Debug;
+use katcp_casper::{Fpga, Listbof, Progdev};
+use log::{debug, error, info, trace, warn};
+use pretty_env_logger;
 use std::{error::Error, net::IpAddr, path::PathBuf};
+use std::{fmt::Debug, net::SocketAddr};
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader, Lines},
     net::{
@@ -36,7 +41,7 @@ struct Args {
     address: IpAddr,
     /// Port of the SNAP katcp tcpborph server
     #[clap(short, long, default_value_t = 7147)]
-    port: u32,
+    port: u16,
     /// Print all log messages and debug information
     #[clap(short, long)]
     verbose: bool,
@@ -47,6 +52,30 @@ struct State {
     lines: Lines<BufReader<OwnedReadHalf>>,
     // The writer
     writer: OwnedWriteHalf,
+}
+
+fn handle_log(log: Log) {
+    if let Log::Inform {
+        level,
+        name,
+        message,
+        ..
+    } = log
+    {
+        match level {
+            LogLevel::Error => error!("{} {}", name, message),
+            LogLevel::Warn => warn!("{} {}", name, message),
+            LogLevel::Info => info!("{} {}", name, message),
+            LogLevel::Debug => debug!("{} {}", name, message),
+            LogLevel::Trace => trace!("{} {}", name, message),
+            _ => println!(
+                "Unexpected Log: {} {} {}",
+                level.to_argument(),
+                name,
+                message
+            ),
+        }
+    }
 }
 
 async fn read_version_connect(state: &mut State) -> VersionConnect {
@@ -61,7 +90,7 @@ async fn read_version_connect(state: &mut State) -> VersionConnect {
         .expect("Error deserializing version connect message")
 }
 
-async fn send_request<K: KatcpMessage>(request: K, state: &mut State) -> (Vec<K>, Vec<Log>)
+async fn send_request<K: KatcpMessage>(request: K, state: &mut State) -> (Vec<K>, Option<Fpga>)
 where
     <K as std::convert::TryFrom<katcp::prelude::Message>>::Error: std::fmt::Debug,
 {
@@ -77,8 +106,10 @@ where
         .expect("Error writting bytes to TCP connection");
 
     // Containers for collected messages
-    let mut logs = vec![];
     let mut informs_and_reply = vec![];
+    // We may recieve a number of FPGA status updates (that seemss to just randomly happen)
+    // We only care about the last one (the current FPGA status)
+    let mut status_updates: Vec<Fpga> = vec![];
 
     // Keep reading lines
     while let Some(line) = state.lines.next_line().await.unwrap() {
@@ -88,9 +119,12 @@ where
         if new_msg_type != request_name && new_msg_type != "log" {
             panic!("Got unexpected KATCP message: {}", raw.name());
         }
-        // Deal with the two cases
+        // Deal with the three cases
         match new_msg_type.as_str() {
-            "log" => logs.push(raw.try_into().expect("Could not deserialize log message")),
+            "log" => handle_log(raw.try_into().expect("Could not deserialize log message")),
+            "fpga" => {
+                status_updates.push(raw.try_into().expect("Could not deserialize fpga stauts"))
+            }
             _ => match raw.kind() {
                 MessageKind::Request => unreachable!(),
                 MessageKind::Reply => {
@@ -104,11 +138,11 @@ where
             },
         }
     }
-    (informs_and_reply, logs)
+    (informs_and_reply, status_updates.last().cloned())
 }
 
 async fn get_bofs(state: &mut State) -> Vec<String> {
-    // Send a listbof message and collect what comes back
+    // Send a listbof message and collect what comes back (no FPGA status updates)
     let (replies, _) = send_request(Listbof::Request, state).await;
     if let Listbof::Reply(IntReply::Ok { num }) = replies.last().unwrap() {
         assert_eq!(
@@ -132,24 +166,31 @@ async fn get_bofs(state: &mut State) -> Vec<String> {
     }
 }
 
+async fn program_bof(filename: String, state: &mut State) -> Result<(), Log> {
+    // Try to program
+    let (reply, status) = send_request(Progdev::Request { filename }, state).await;
+    todo!()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // Start the logger
+    pretty_env_logger::init();
     // Grab the command line arguments
     let args = Args::parse();
     // Connect to the SNAP katcp server
-    let (reader, writer) = TcpStream::connect("10.10.1.3:7147").await?.into_split();
+    let (reader, writer) = TcpStream::connect(SocketAddr::new(args.address, args.port))
+        .await?
+        .into_split();
     // Setup the program state
     let mut state = State {
         lines: BufReader::new(reader).lines(),
         writer,
     };
     // Read the first three informs that give us system information
-    let lib = read_version_connect(&mut state).await;
-    println!("Got lib");
-    let protocol = read_version_connect(&mut state).await;
-    println!("Got protocol");
-    let kernel = read_version_connect(&mut state).await;
-    println!("{:#?}\n{:#?}\n{:#?}", lib, protocol, kernel);
+    let _ = read_version_connect(&mut state).await;
+    let _ = read_version_connect(&mut state).await;
+    let _ = read_version_connect(&mut state).await;
     // Perform the action
     match args.command {
         Command::Load { .. } => {
